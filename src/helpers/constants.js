dba4d20ec9e8c288f1ec4e2acb14eef6296aabf1
Pubudu Dodangoda <pubudu.dodangoda@trycake.com>
2018-01-21 10:27:48 +0530

    Expose all constants in the app using a single proxy module
    Constant files can exist anywhere, but by exporting all constants here, importing constants become trivial

---
+++ src/helpers/constants.js
@@ -0,0 +1,7 @@
+/**
+ * Created by pubudud on 1/20/18.
+ */
+
+export * from '../utils/serverConfigs';
+export * from '../utils/configConstants';
+export * from '../utils/httpStatusCodes';
---
+++ src/utils/httpStatusCodes.js
@@ -0,0 +1,7 @@
+
+export const SUCCESS = 200;
+export const INTERNAL_ERROR = 500;
+export const BAD_REQUEST = 400;
+export const UNAUTHORIZED = 401;
+export const FORBIDDEN = 403;
+export const NOT_FOUND = 404;
