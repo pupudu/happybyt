dcd8489b3e57f879233ad1f721f8e11013b9c054
Pubudu Dodangoda <pubudu.dodangoda@trycake.com>
2018-01-21 10:49:21 +0530

    Fix issue in gulp config urls

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ const concat = require("gulp-concat");
 const sass = require('gulp-sass');
 
 gulp.task("babel", gulp.series(function () {
-    return gulp.src(["src/**/*.js", "!src/web-app/**"])
+    return gulp.src(["src/**/*.js"])
         .pipe(babel())
         .pipe(gulp.dest("dist"));
 }));
 
 gulp.task("babel-source-maps", gulp.series(function () {
-    return gulp.src(["src/**/*.js", "!src/web-app/**"])
+    return gulp.src(["src/**/*.js"])
         .pipe(sourceMaps.init())
         .pipe(babel())
         .pipe(sourceMaps.write("."))
@@ -19,23 +19,23 @@ gulp.task("babel-source-maps", gulp.series(function () {
 }));
 
 gulp.task("clone-react-app", gulp.series(function () {
-    return gulp.src("src/web-app/build/**/*")
+    return gulp.src("web-app/build/**/*")
         .pipe(gulp.dest("dist/web-app"));
 }));
 
 gulp.task('sass', gulp.series(function () {
-    return gulp.src('./src/web-app/scss/style.scss')
+    return gulp.src('./web-app/scss/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(concat('style.css'))
-        .pipe(gulp.dest('./src/web-app/public/css'))
+        .pipe(gulp.dest('./web-app/public/css'))
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(concat('style.min.css'))
-        .pipe(gulp.dest('./src/web-app/public/css'));
+        .pipe(gulp.dest('./web-app/public/css'));
 }));
 
 // Watching SCSS files
 gulp.task('sass:watch', gulp.series(function () {
-    gulp.watch('./src/web-app/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('./web-app/scss/**/*.scss', gulp.series('sass'));
 }));
 
 gulp.task("dev", gulp.series("babel-source-maps", "clone-react-app"));
